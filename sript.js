f8b8f768b59d3227d46d675922dd54ef4e2f9606
felipedias <dias20felipe@gmail.com>
2025-07-04 10:53:32 -0300

    variaveis

---
+++ sript.js
@@ -0,0 +1,93 @@
+// VARIÁVEL LET E VAR 
+
+
+// Declara uma variável sem valor
+
+var carro =
+
+console.log(carro)
+
+
+// Declara uma variável com valor
+
+var barco = "Navio"
+
+console.log(barco)
+
+
+// Substituir o valor da variável
+
+barco = "Navio cargueiro "
+
+console.log(barco)
+
+// Case sensitive
+
+/*
+Case Sensitive
+Nesta aula, abordo o conceito de "case sensitive" em linguagens de programação, destacando a importância de diferenciar letras maiúsculas e minúsculas. 
+Demonstro na prática como a sensibilidade a maiúsculas e minúsculas afeta a criação e manipulação de variáveis. 
+Exploro exemplos de declaração de variáveis com nomes semelhantes, porém diferenciados por letras maiúsculas e minúsculas, evidenciando como isso influencia o código. 
+Destaco a sobrescrição de variáveis com o mesmo nome e destaco a importância de compreender esse comportamento ao programar.
+
+*/
+
+
+/* aprendemos sobre a declaração de variáveis utilizando "let" em JavaScript. 
+A diferença principal em relação ao "var" é que o "let" não permite a redeclaração de variáveis com o mesmo nome, evitando sobrescrita de conteúdo. 
+Além disso, foi explicado como as variáveis funcionam na memória, sendo um espaço reservado com um nome como referência. 
+O "let" é uma palavra reservada do JavaScript para criar variáveis.   */
+
+
+
+// Let
+
+// Declara uma variavel sem valor 
+
+let user 
+
+console.log("Felipe Dias Dos Santos") 
+
+
+// Declara uma variável com valor
+let email = "dias200felipe" 
+
+email = "dias100felipe@gmail.com"
+
+
+
+
+
+
+/* 
+Const.
+Quando precisamos armazenar um valor que não deve ser alterado, podemos usar uma constante em JavaScript. 
+Assim como uma variável, uma constante guarda um valor temporariamente na memória, porém esse valor é fixo e não pode ser modificado. 
+Ao tentar alterar o valor de uma constante, um erro será gerado. 
+É útil utilizar constantes quando queremos garantir que um valor não seja alterado. 
+Essa prática será explorada ao longo das aulas, principalmente durante o desenvolvimento de projetos.  
+*/
+
+
+
+
+
+// Const 
+
+//  Uma constante cria uam variável cujo valor é fixo (não pode ser alterado)
+
+const nome =  "Felipe Dias"
+
+console.log(nome)
+
+// Irá gerar um erro porque o valor não pode ser alterado
+
+nome =  "Felipe D"
+
+console.log(nome)
+
+
+
+
+
+
